ee3c969ff07ee46610ef79863e0162e0b4467c41
xsonykkx <sofiya.strologo@gmail.com>
2024-07-06 03:07:08 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+my_string = input('Введите текст: ')
+lenght_of_my_string = len(my_string)
+print('В введённом тексте' , lenght_of_my_string , 'символов')
+
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
